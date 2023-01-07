2dfd101b102276e3b6883c1cad82497cb607f4d1
Svitlana D <svetk2801@gmail.com>
2023-01-07 12:48:22 +0200

    Module 3 Task 6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task6;
-
+    //You will be given a float number, and your task will be to find
+    // a correct way to round this variable to integer variable,
+    // corresponding to Math rules (requires googling). Print that variable to console.
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int result = Math.round(numberToBeRounded);
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -1,5 +1,7 @@
 package lang.print.gaps.task6;
-
+    // You will be given a code snippet, and you task will be to get
+    // expected result by inserting correct pre/post increment oprators to specified variables.
+    // Expected results: x is expected to be 40, the rest but b and d to be incremented.
 public class PrePostIncrement {
     public static void main(String[] args) {
         int a = 5;
@@ -8,7 +10,7 @@ public class PrePostIncrement {
         int d = 15;
 
         // use post-increment operator in the mathematical expression  
-        int x =   a   + b +   c   + d;
+        int x =   a++   + b +   ++c   + d;
         System.out.println("Expected x is 40, x = " + x);
         // print the updated value of a, b, c, and d  
         System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
